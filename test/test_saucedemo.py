92e65379bdc88edb5ff6cb380e2df932f7a3ec35
Gabriela Irrazabal <gabiirrazabal@gmail.com>
2025-10-25 02:04:57 -0300

    subida de los primeros archivos de la preEntrega

---
+++ test/test_saucedemo.py
@@ -0,0 +1,31 @@
+import pytest
+
+
+
+
+@pytest.fixture
+def driver():
+    # configuracion para consultar a selenium webdriver
+
+def test_login():
+    # logeo de usuario con username y password
+    # click al botón de login
+
+    #redirija a la página de inventario
+
+    #verificar el título de la página
+
+def test_catalogo():
+  # logeo de usuario con username y password
+    # click al botón de login
+
+    #verificar el titulo de la pagina html
+    # comprobar si existen productos en la página visibles (len())
+    # verificar elementos importntes en la pagina
+
+def test_carrito():
+    # logeo de usuario con username y password
+    # click al botón de login
+    #llevarme a la pagina del carrito de compras
+#incremento de carrito al agregar un producto
+#comprobar que el produzco aparezca en el carrito correctamente
